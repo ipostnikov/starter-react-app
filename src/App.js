329e140e090df01d54c76d6624fddf7597d3fbbf
Ihor <i.postnikov97@gmail.com>
2023-04-13 03:32:41 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+           <code>Ihor Postnikov</code> <a href="http://ihpst.byethost15.com/"> DevOps Terminal </a>
         </p>
         <a
           className="App-link"
