bfe19dc86b46d75de20eeeaee2d4789c4afff97a
Ihor <i.postnikov97@gmail.com>
2023-04-13 03:33:29 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-           <code>Ihor Postnikov</code> <a href="http://ihpst.byethost15.com/"> DevOps Terminal </a>
+           Ihor <code> Postnikov</code>
         </p>
         <a
           className="App-link"
